992226cdb2bc2df92d895f0281503064c5745649
mogyi26b <tydunu@hallgato.nje.hu>
2025-06-12 10:45:37 +0200

    Add files via upload

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=oltopont', 'root', '',
+    $dbh = new PDO('mysql:host=localhost;dbname=demoserv_oltopont', 'mzperx', 'mzperx756',
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]);
 } catch (PDOException $e) {
     die('Adatbázis kapcsolat hiba: ' . $e->getMessage());
